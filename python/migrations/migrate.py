6aad9b81f97290253374f8549e8aeb81e890920f
Rudy Fisher <rfisherd7@gmail.com>
2025-07-26 14:19:34 -0500

    removed db file when downgrading migration

--- python/migrations/migrate.py
+++ python/migrations/migrate.py
@@ -1,9 +1,11 @@
 import sys
+import os
 import sqlite3
 import python.database_utilities.util as util
 from types import ModuleType
 
 args: list[str] = ["v1", "upgrade"]
+database_file_path: str = "database.db"
 
 arg_len: int = len(sys.argv)
 for i in range(len(args)):
@@ -14,9 +16,14 @@ modules: list[ModuleType] = []
 
 modules.append(util.import_migration_module(args[0], "m001_create_tables"))
 
-connection: sqlite3.Connection = sqlite3.connect("database.db")
+if args[1] == "upgrade":
+    if not os.path.exists(database_file_path):
+        with open(database_file_path, "w") as file:
+            pass
 
-if (args[1] == "downgrade"):
+connection: sqlite3.Connection = sqlite3.connect(database_file_path)
+
+if args[1] == "downgrade":
     for module in reversed(modules):
         module.downgrade(connection)
 else:
@@ -24,3 +31,7 @@ else:
         module.upgrade(connection)
 
 connection.close()
+
+if args[1] == "downgrade":
+    if os.path.exists(database_file_path):
+        os.remove(database_file_path)
