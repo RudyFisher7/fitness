26663ef771033d9e75a4c0cd5c3b34a0d217c5f5
Rudy Fisher <rfisherd7@gmail.com>
2025-07-27 18:49:06 -0500

    added more queries

---
+++ sql/v1/queries/average_set_count_per_exercise.sql
@@ -0,0 +1,9 @@
+WITH set_count AS (
+	SELECT routine_id AS r_id, exercise_id AS e_id, COUNT(*) AS set_count FROM routine_exercise_set res
+	GROUP BY routine_id, exercise_id
+	ORDER BY routine_id
+)
+SELECT r_id, e_id, AVG(set_count.set_count)
+FROM set_count
+GROUP BY set_count.e_id
+;
